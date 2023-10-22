a16d18a4d1efc79da8a1e136efe8cfcef3162362
HP <thathsara.hewage99@gmail.com>
2023-10-22 23:41:09 +0530

    db connection error temporary solved

--- utils/database/mongoose.ts
+++ utils/database/mongoose.ts
@@ -5,9 +5,9 @@ let isConnected = false;
 export const DBconnection = async () => {
   mongoose.set("strictQuery", true);
 
-  if (!process.env.MONGO_URL_DEV) {
-    return console.log("MONGODB URL IS MISSING !");
-  }
+  // if (!process.env.MONGO_URL_DEV) {
+  //   return console.log("MONGODB URL IS MISSING !");
+  // }
 
   if (isConnected) {
     console.log("MONGODB CONNECTION IS ALREADY ESTABLISHED !");
@@ -15,7 +15,7 @@ export const DBconnection = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGO_URL_DEV);
+    await mongoose.connect("mongodb://localhost:27017/verse");
     isConnected = true;
     console.log("MONGODB CONNECTED !");
   } catch (err) {
